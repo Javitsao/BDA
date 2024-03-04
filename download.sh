b3a339c1d52159de65d51fb35fce533e3492a4b4
Javitsao <112849221+Javitsao@users.noreply.github.com>
2024-03-04 22:34:30 +0800

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 mkdir videos
-wget 'https://docs.google.com/uc?export=download&id=1_JsBF65mnvZo-eCx5U6s9gmx36LiAvOk' -O videos/1.mp4
-wget 'https://docs.google.com/uc?export=download&id=1R7T6BlfZ7zfHgOe6lR7k0s7RjiVOXjts' -O videos/2.mp4
+wget 'https://docs.google.com/uc?export=download&id=1_JsBF65mnvZo-eCx5U6s9gmx36LiAvOk' -O videos/RPReplay_Final1709555946.mp4
+wget 'https://docs.google.com/uc?export=download&id=1R7T6BlfZ7zfHgOe6lR7k0s7RjiVOXjts' -O videos/RPReplay_Final1709556080.mp4
