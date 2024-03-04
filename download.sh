e53f1653b8abdc907cffb9acb5bdaece53cc8398
Javitsao <112849221+Javitsao@users.noreply.github.com>
2024-03-04 22:20:44 +0800

    Add files via upload

--- download.sh
+++ download.sh
@@ -1,1 +1,4 @@
-gdown https://drive.google.com/drive/folders/1XicB_g9V06cwDP_hPnjPa0sPBhmbZwSF?usp=sharing -O . --folder
+#! /usr/bin/env bash
+mkdir videos
+wget 'https://docs.google.com/uc?export=download&id=1_JsBF65mnvZo-eCx5U6s9gmx36LiAvOk' -O videos/1.mp4
+wget 'https://docs.google.com/uc?export=download&id=1R7T6BlfZ7zfHgOe6lR7k0s7RjiVOXjts' -O videos/2.mp4
